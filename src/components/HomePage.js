5952f496133c49ffb1eccce20242515492b058e1
peterx <xu.kang@husky.neu.edu>
2020-10-30 19:46:24 -0400

    added basic search component

---
+++ src/components/HomePage.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {searchService} from "../services/search";
+
+class HomePage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchContent: '',
+            searchResult: ''
+        }
+    }
+    onSearch = () => {
+        new searchService().searchSong(this.state.searchContent)
+            .then((data) => {
+                console.log(data)
+                this.setState({searchResult: data})
+            })
+
+    }
+    render() {
+        return (
+            <div>
+                <input onChange={e => this.setState({searchContent: e.target.value})}/>
+                <button onClick={() => this.onSearch()}>Search</button>
+                <h1>{this.state.searchResult != "" ? this.state.searchResult.map(a => a.name) : null}</h1>
+
+            </div>
+        );
+    }
+}
+
+HomePage.propTypes = {};
+
+export default HomePage;
---
+++ src/services/search.js
@@ -0,0 +1,34 @@
+export class searchService {
+
+    constructor() {
+
+    }
+
+    searchSong = (searchContent) => {
+        console.log(searchContent)
+        let t = {content: searchContent}
+       return  fetch('http://localhost:8887/search', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(t)
+        }) .then(response => {
+            console.log(123)
+           let t = response.json()
+           console.log(t)
+           return t;
+       })
+            .then(response => {
+                console.log(response)
+                return response;
+            })
+            .catch((error) =>  console.log("Error message is ", error))
+
+    }
+
+
+
+
+
+
+}
+
